254e6b1a6fc1559b0f2a382d125e0a0120a4cc3d
ZhuGG <h.bettollo@gmail.com>
2025-09-24 13:05:50 +0200

    fix: set relative base for local builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-  base: '/CheckBadges/',
+  base: isGitHubPages ? '/CheckBadges/' : './',
   plugins: [react()],
   build: {
     target: 'esnext'
